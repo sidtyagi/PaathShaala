3a062a1a9ea9aee25d23b6c9d8d6d985a04349c2
Siddharth Tyagi (sidtyagi) <sidtyagi@cisco.com>
2017-05-27 12:32:37 +0530

    added the regional server to ip mapping

--- DHCP Regional ping check/dhcp_test.py
+++ DHCP Regional ping check/dhcp_test.py
@@ -1,4 +1,5 @@
 def fetch_region_code(line):
+    print line
     region_code=line.split(',')[0].split('-')[1]
     return region_code
 
@@ -16,3 +17,9 @@ if __name__=='__main__':
     dhcp_mapping=create_dhcp_mapping('Mapping.txt')
     print dhcp_mapping
         
+    #ping the dest ip based on the regional server mapping
+    f=open('ip_list.txt')
+    for line in f:
+        region_code=fetch_region_code(line)
+        dhcp_server=dhcp_mapping[region_code]
+        print 'the corresponding dhcp server %s'%dhcp_server

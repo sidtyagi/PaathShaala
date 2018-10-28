48b05908dbe6a02482838dc3c223d2cf2656c870
sidtyagi <mailsiddharthtyagi@gmail.com>
2018-10-28 22:49:57 +0530

    added SO test

---
+++ Trash/U/StackOverflow/StackOverflow/Post.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow
+{
+    class Post
+    {
+        private string _title;
+        private string _description;
+        private DateTime _dateTime;
+        private int _vote = 0;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+
+            set
+            {
+                _title = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+
+            set
+            {
+                _description = value;
+            }
+        }
+
+        public DateTime DateTime
+        {
+            get
+            {
+                return _dateTime;
+            }
+
+            set
+            {
+                _dateTime = value;
+            }
+        }
+
+        public void upvote()
+        {
+            _vote += 1;
+        }
+
+        public void downvote()
+        {
+            _vote -= 1;
+        }
+
+        public int display()
+        {
+            return _vote;
+        }
+    }
+}
---
+++ Trash/U/StackOverflow/StackOverflow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Post();
+            obj.Title = "First SO Post";
+            obj.Description = "Test Description";
+            obj.DateTime = DateTime.Now;
+            Console.WriteLine("Upvoting thrice");
+            for (int a =1; a<=3;a=a+1)
+            {
+                obj.upvote();
+            }
+            Console.WriteLine($"Current vote{obj.display()}");
+            Console.WriteLine($"Now trying downvote");
+            obj.downvote();
+            Console.WriteLine($"Current vote{obj.display()}");
+            Console.WriteLine($"Title {obj.Title}");
+            Console.WriteLine($"Description {obj.Description}");
+            Console.WriteLine($"Create time {obj.DateTime}");
+            Console.ReadLine();
+        }
+    }
+}

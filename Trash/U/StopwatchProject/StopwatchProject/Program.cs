3f159600de6b1cea8e4be81262989dc9b3f077a2
sidtyagi <mailsiddharthtyagi@gmail.com>
2018-10-28 13:22:02 +0530

    test for start stop

---
+++ Trash/U/StopwatchProject/StopwatchProject/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StopwatchProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StopWatch obj = new StopWatch();
+
+                var startTime = obj.Start().ToString();
+                Thread.Sleep(2000);
+                var endTime = obj.Stop().ToString();
+                var duration = DateTime.Parse(endTime).Subtract(DateTime.Parse(startTime));
+                Console.WriteLine(duration);
+
+                // Attempt 2 for restart
+                Console.WriteLine("Start again");
+                startTime = obj.Start().ToString();
+                Console.WriteLine("Sleep for 3 sec");
+                Thread.Sleep(3000);
+                Console.WriteLine("Stopping");
+                endTime = obj.Stop().ToString();
+                duration = DateTime.Parse(endTime).Subtract(DateTime.Parse(startTime));
+                Console.WriteLine(duration);
+
+
+                // Attempt 3 for start when already started
+                Console.WriteLine("Start again when already started");
+                startTime = obj.Start().ToString();
+                startTime = obj.Start().ToString();
+                Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error encountered {ex.Message}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Trash/U/StopwatchProject/StopwatchProject/StopWatch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopwatchProject
+{
+    class StopWatch
+    {
+        private bool _state;
+        public DateTime Start()
+        {
+            if (_state)
+            {
+                Console.WriteLine("Alredy Started, hence cannot restart");
+                throw new InvalidOperationException();
+            }
+            _state = true;
+            var CurDate = DateTime.Now;
+            Console.WriteLine("Started");
+            return CurDate;
+        }
+        public DateTime Stop()
+        {
+            if(_state)
+            {
+                var CurDate = DateTime.Now;
+                Console.WriteLine("Attempting to Stop");
+                Console.WriteLine("Stopped");
+                _state = false;
+                return CurDate;
+            }
+            Console.WriteLine("Cannot stop as the watch is not yet started");
+            throw new InvalidOperationException();
+        }
+
+        
+    }
+}
